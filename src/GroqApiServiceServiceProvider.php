42a2efd2dcf956e0d76e4bb0137da3f96db68c48
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2025-08-12 18:33:58 +0100

    corrige

--- src/GroqApiServiceServiceProvider.php
+++ src/GroqApiServiceServiceProvider.php
@@ -12,8 +12,6 @@ class GroqApiServiceServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         $package->name($this->name)
-            ->hasTranslations()
-            ->hasViews()
             ->hasMigrations([
                 'create_groq-api-services-settings'
             ]);
@@ -25,7 +23,6 @@ class GroqApiServiceServiceProvider extends PackageServiceProvider
 
         $this->publishes([
             __DIR__ . '/pages/CustomManagerGroqApiServiceSettings.php' => app_path('Filament/Pages/ManagerGroqApiServiceSettings.php'),
-            __DIR__ . '/database/migrations/create_groq-api-services-settings.php' => database_path('migrations/2025_01_01_000000_create_groq-api-services-settings.php'),
         ], $this->name);
     }
 }
